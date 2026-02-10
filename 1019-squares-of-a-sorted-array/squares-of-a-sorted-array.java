dfb25ca5a4efdb50bc58617c502769112c0df096
Sanjeev kumar <43263444+sanjeevkr07@users.noreply.github.com>
2026-02-11 00:49:00 +0530

    Time: 1 ms (99.80%) | Memory: 47.2 MB (86.48%) - LeetSync

--- 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.java
+++ 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.java
@@ -2,33 +2,25 @@ class Solution {
     public int[] sortedSquares(int[] nums) {
         int n = nums.length;
         int[] result = new int[n];
-        int foldingPoint = -1;
-        //[0,2,3,4,5]
 
-        for(int i = 0; i < n; i++){
-            if(nums[i]>=0 && foldingPoint<0) foldingPoint = i;
-            nums[i]=nums[i]*nums[i];
+        int left = 0;
+        int right = n-1;
+        int k = n-1;
+
+        while(left<=right){
+            int leftSqr = nums[left]*nums[left];
+            int rightSqr = nums[right]*nums[right];
+
+            if(leftSqr > rightSqr){
+                result[k--] = leftSqr;
+                left++;
+            }
+            else {
+                result[k--] = rightSqr;
+                right--;
+            }
         }
-        // System.out.println(foldingPoint);
-
-        if(foldingPoint==-1) foldingPoint=n-1;
-
-        int i = foldingPoint-1;
-        int j = foldingPoint;
-        int k = 0;
-
-        while(i>=0 && j<n){
-            if(nums[i]<nums[j])
-                result[k++] = nums[i--];
-            else
-                result[k++] = nums[j++];
-
-        }
-
-        while(j<n) result[k++] = nums[j++];
-        while(i>=0) result[k++] = nums[i--];
-
-        return result;
         
+        return result;
     }
 }
