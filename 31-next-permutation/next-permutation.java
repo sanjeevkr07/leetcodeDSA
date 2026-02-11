5ae88801b10296b493ede60ca50909292bc036de
Sanjeev kumar <43263444+sanjeevkr07@users.noreply.github.com>
2026-02-11 22:56:50 +0530

    Time: 3 ms (3.54%) | Memory: 45 MB (13.59%) - LeetSync

--- 31-next-permutation/next-permutation.java
+++ 31-next-permutation/next-permutation.java
@@ -1,44 +1,60 @@
 class Solution {
-    public void reverse(int[] nums,int a,int b){
-        while(a<b){
-            //swap(nums[a],nums[b]);
-            int temp=nums[a];
-            nums[a]=nums[b];
-            nums[b]=temp;
-            a++;
-            b--;
-        }
-    }
     public void nextPermutation(int[] nums) {
+        
+        // [4,1,2,6,8,2]=>[4,1,2,8,2,6]
+        // [3,2,1]=>[1,2,3]
+        //[7,3,2,9,0]=[7,3,9,0,2]=>[7,3,9,2,0]=>[7,9,0,2,3]
+        //[4,3,2,1]=>[]
+        //[1, 3, 5, 4, 2]=> [1,5,2,3,4]
 
-        int ind=-1;
-        int n=nums.length;
-        //find thr break point
+        //find pivot and interchange pivot with smallest among all biggest numebers than pivot in the right and reverse array from pivot+1 to end
+        //if not pivot found reverse entire array
 
-        for(int i=n-2;i>=0;i--){
+        int pivot = -1;
+        int n = nums.length;
+        for(int i = n-2 ; i>=0 ; i--){
             if(nums[i]<nums[i+1]){
-                ind=i;
+                pivot = i ;
                 break;
             }
         }
-        if(ind==-1){
-            reverse(nums,0,n-1);
-        }
+        System.out.println(pivot);
+
+        if(pivot == -1) reverse(nums,0,n-1);
+
         else{
-            for(int i=n-1;i>ind;i--){
-                if(nums[ind]<nums[i]){
-                    //swap(nums[ind],nums[i]);
-                    int temp=nums[ind];
-                    nums[ind]=nums[i];
-                    nums[i]=temp;
+            //find swapper with pivot 
+            int swapperPos = -1;
+
+            for(int i = n-1; i>=0 ; i--){
+                if(nums[i]>nums[pivot]){
+                    swapperPos = i;
                     break;
                 }
             }
 
-            reverse(nums,ind+1,n-1);
+            //swap with swapper
+            int temp = nums[pivot];
+            nums[pivot] = nums[swapperPos];
+            nums[swapperPos] = temp;
+
+            reverse(nums,pivot+1,n-1);
+
         }
 
 
-        
+
+
+
+    }
+
+    private static void reverse(int[] nums, int i, int j){
+        while(i<j){
+            int temp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = temp;
+            i++;
+            j--;
+        }
     }
 }
