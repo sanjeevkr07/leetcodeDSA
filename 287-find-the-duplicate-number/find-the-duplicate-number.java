5b23ef080c0f18389f25546b81acf9451ecee6fd
Sanjeev kumar <43263444+sanjeevkr07@users.noreply.github.com>
2026-02-15 00:44:03 +0530

    Time: 5 ms (61.04%) | Memory: 83.1 MB (43.78%) - LeetSync

--- 287-find-the-duplicate-number/find-the-duplicate-number.java
+++ 287-find-the-duplicate-number/find-the-duplicate-number.java
@@ -3,23 +3,40 @@ class Solution {
 
         int slow = 0;
         int fast = 0;
-        int n = nums.length;
 
         do{
-            // System.out.println("Before: slow: "+slow+" fast: "+fast);
             slow = nums[slow];
             fast = nums[nums[fast]];
-            // System.out.println("After: slow: "+slow+" fast: "+fast);
-        }while(slow!=fast);
-
-        slow =0;
-
-        while(slow!=fast){
-            slow=nums[slow];
-            fast=nums[fast];
+        }while(fast!=slow);
+        fast = 0;
+        while(fast!=slow){
+            slow = nums[slow];
+            fast = nums[fast];
         }
 
         return slow;
-        
     }
 }
+// fast = 0 | slow = 0
+// // [1,3,4,2,2]
+
+// slow = nums[slow] = 1
+// fast = nums[nums[fast]] =nums[1] = 3
+// __________________________________
+// fast = 1 | slow = 3
+
+// slow = nums[3] = 2
+// fast = nums[nums[1]] =nums[3] = 2
+// __________________________________
+// 2
+// slow = nums[4] = 2
+// fast = nums[nums[4]] =nums[2] = 4
+// __________________________________
+// 3
+// slow = nums[2] = 4
+// fast = nums[nums[2]] =nums[4] = 2
+// __________________________________
+// 4
+// slow = nums[2] = 4
+// fast = nums[nums[2]] =nums[4] = 2
+// __________________________________
