e91ab57007e4ab55878556f0baf1ebb648211b8a
Sanjeev kumar <43263444+sanjeevkr07@users.noreply.github.com>
2026-02-23 23:57:48 +0530

    Time: 8 ms (90.66%) | Memory: 49.1 MB (72.00%) - LeetSync

--- 56-merge-intervals/merge-intervals.java
+++ 56-merge-intervals/merge-intervals.java
@@ -4,26 +4,26 @@ class Solution {
         Arrays.sort(intervals,(a,b)->Integer.compare(a[0],b[0]));
         
         int m = intervals.length;
-        int n = intervals[0].length;
+        // int n = intervals[0].length;
 
         List<int[]> result = new ArrayList<>();
         
-        int[] interval = intervals[0];
+        // int[] interval = intervals[0];
         int start = intervals[0][0];
         int end = intervals[0][1];
         int i = 1 ; 
         while(i < m){
             int[] currInterval = intervals[i];
             // System.out.println("end: "+end+" comparing: "+ currInterval[0]+ " "+currInterval[1]);
-            if(currInterval[0] <= interval[1] || end >= currInterval[0]){
+            if(end >= currInterval[0]){
                 end = Math.max(end,currInterval[1]);
             }
             else{
                
                 result.add(new int[]{start,end});
-                interval = currInterval;
-                start = interval[0];
-                end = interval[1];
+                // interval = currInterval;
+                start = currInterval[0];
+                end = currInterval[1];
                 // System.out.println(result);
             }
             i++;
