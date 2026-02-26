a7770c3fdfe7b82c7d45d2f7366ea440fab77674
Sanjeev kumar <43263444+sanjeevkr07@users.noreply.github.com>
2026-02-26 22:51:18 +0530

    Time: 3 ms (87.50%) | Memory: 43.4 MB (58.28%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.java
+++ 20-valid-parentheses/valid-parentheses.java
@@ -1,26 +1,26 @@
+// import java.util.function.Predicate;
 class Solution {
     public boolean isValid(String s) {
         Stack<Character> st = new Stack<>();
-
-        for (char c : s.toCharArray()) {
-            if (c == '(' || c == '{' || c == '[') {
-                st.push(c);
-            } 
-            else { // closing bracket
-                if (st.isEmpty()) return false;
-
+        // Predicate<Character> closing = c-> c==']' || c==')' || c == '}';
+        // int[] closingArr = {'}',']',')'};
+        // int[] openingArr = {'{','[','('};
+        for(char c: s.toCharArray()){
+            if(c=='{' || c=='(' || c=='[' ) st.push(c);
+            else{
+                if(st.isEmpty()) return false;
                 char top = st.peek();
 
                 if ((c == ')' && top == '(') ||
                     (c == '}' && top == '{') ||
                     (c == ']' && top == '[')) {
                     st.pop();
-                } else {
-                    return false;
-                }
+                    } 
+                else return false;
             }
+            
         }
-
         return st.isEmpty();
+        
     }
 }
