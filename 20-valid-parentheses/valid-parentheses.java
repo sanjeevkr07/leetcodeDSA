011552ae1d57b64fdd8f11f885996a8d47a2c988
Sanjeev kumar <43263444+sanjeevkr07@users.noreply.github.com>
2026-02-26 22:40:32 +0530

    Time: 4 ms (42.21%) | Memory: 43.6 MB (26.37%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.java
@@ -0,0 +1,25 @@
+import java.util.function.Predicate;
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> st = new Stack<>();
+        Predicate<Character> closing = c-> c==']' || c==')' || c == '}';
+        int[] closingArr = {'}',']',')'};
+        int[] openingArr = {'{','[','('};
+        for(char c: s.toCharArray()){
+            if(closing.test(c) && !st.isEmpty()){
+                char ch = st.peek();
+                if( (c==closingArr[0] && ch==openingArr[0]) ||
+                    (c==closingArr[1] && ch==openingArr[1]) ||
+                    (c==closingArr[2] && ch==openingArr[2])    ) st.pop();
+                    
+                else return false;
+            }
+            else{
+                st.push(c);
+            }
+        }
+
+        return st.isEmpty();
+        
+    }
+}
