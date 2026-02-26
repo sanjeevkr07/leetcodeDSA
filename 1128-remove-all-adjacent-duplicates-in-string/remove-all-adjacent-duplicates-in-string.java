40161336aa15c36020c3d1057f95b10c7ab5eaae
Sanjeev kumar <43263444+sanjeevkr07@users.noreply.github.com>
2026-02-26 22:08:27 +0530

    Time: 29 ms (42.92%) | Memory: 47.1 MB (73.77%) - LeetSync

--- 1128-remove-all-adjacent-duplicates-in-string/remove-all-adjacent-duplicates-in-string.java
+++ 1128-remove-all-adjacent-duplicates-in-string/remove-all-adjacent-duplicates-in-string.java
@@ -1,29 +1,17 @@
 class Solution {
     public String removeDuplicates(String s) {
-        
-        StringBuilder str = new StringBuilder(s);
+        Stack<Character> st = new Stack<>();
 
-        int i = 0;
-
-        while(i<str.length()-1){
-    
-            //System.out.println("i at (START):"+i);
-            if(str.charAt(i) == str.charAt(i+1)){
-                //System.out.println("char matched: "+str.charAt(i)+ " "+str.charAt(i+1));
-                str.deleteCharAt(i+1);
-                str.deleteCharAt(i);
-                
-                //System.out.println("new String :"+str);
-                if(i!=0) i--;
+        for(char c : s.toCharArray()){
+            if(!st.isEmpty() && st.peek() == c){
+                st.pop();
             }
-            else {
-               // System.out.println("No Match found increment");
-                i++;
-            }
-           // System.out.println("i at (END):"+i);
-
+            else st.push(c);
         }
-        //System.out.println(str);
-        return str.toString();
+        StringBuilder res = new StringBuilder();
+        for(char c: st){
+            res.append(c);
+        }
+        return res.toString();
     }
 }
