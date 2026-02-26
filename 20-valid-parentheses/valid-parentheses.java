a365db439c3beea23d1b80659624d3a586513af6
Sanjeev kumar <43263444+sanjeevkr07@users.noreply.github.com>
2026-02-26 22:46:11 +0530

    Time: 4 ms (42.21%) | Memory: 43.3 MB (73.94%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.java
+++ 20-valid-parentheses/valid-parentheses.java
@@ -3,18 +3,19 @@ class Solution {
     public boolean isValid(String s) {
         Stack<Character> st = new Stack<>();
         Predicate<Character> closing = c-> c==']' || c==')' || c == '}';
-        int[] closingArr = {'}',']',')'};
-        int[] openingArr = {'{','[','('};
+        // int[] closingArr = {'}',']',')'};
+        // int[] openingArr = {'{','[','('};
         for(char c: s.toCharArray()){
             if(closing.test(c) && !st.isEmpty()){
                 char ch = st.peek();
-                if( (c==closingArr[0] && ch==openingArr[0]) ||
-                    (c==closingArr[1] && ch==openingArr[1]) ||
-                    (c==closingArr[2] && ch==openingArr[2])    ) st.pop();
-                    
+                char top = st.peek();
+
+                if ((c == ')' && top == '(') ||
+                    (c == '}' && top == '{') ||
+                    (c == ']' && top == '[')) {
+                    st.pop();
+                    }      
                 else return false;
-            }else if(closing.test(c) && st.isEmpty()){
-                return false;
             }
             else{
                 st.push(c);
