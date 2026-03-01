1f650a5f3204f8595882f850040149c66f14e20a
Sanjeev kumar <43263444+sanjeevkr07@users.noreply.github.com>
2026-03-02 01:01:25 +0530

    Time: 0 ms (100.00%) | Memory: 45.5 MB (51.26%) - LeetSync

--- 83-remove-duplicates-from-sorted-list/remove-duplicates-from-sorted-list.java
+++ 83-remove-duplicates-from-sorted-list/remove-duplicates-from-sorted-list.java
@@ -14,7 +14,7 @@ class Solution {
         ListNode curr = head;
 
         while(curr!=null && curr.next!=null){
-            System.out.println(curr.val);
+            // System.out.println(curr.val);
             while(curr.next!=null && curr.val == curr.next.val){
                 
                     curr.next = curr.next.next;
