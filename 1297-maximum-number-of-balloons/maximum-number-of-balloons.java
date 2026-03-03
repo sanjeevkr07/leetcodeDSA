2c7da0a5e86ff2e45d840ad96dd4ce949af30c6a
Sanjeev kumar <43263444+sanjeevkr07@users.noreply.github.com>
2026-03-03 23:16:05 +0530

    Time: 2 ms (96.14%) | Memory: 43.3 MB (88.44%) - LeetSync

--- 1297-maximum-number-of-balloons/maximum-number-of-balloons.java
+++ 1297-maximum-number-of-balloons/maximum-number-of-balloons.java
@@ -1,20 +1,22 @@
 class Solution {
     public int maxNumberOfBalloons(String text) {
-        HashMap<Character,Integer>  mp = new HashMap<>();
 
-        for(char c:text.toCharArray()){
-            if(c == 'b' || c == 'a' || c == 'l' || c == 'o' || c == 'n' )
-            mp.put(c,mp.getOrDefault(c,0)+1);
+        int b = 0, a = 0, l = 0, o = 0, n = 0;
+
+        for (char c : text.toCharArray()) {
+            if (c == 'b') b++;
+            else if (c == 'a') a++;
+            else if (c == 'l') l++;
+            else if (c == 'o') o++;
+            else if (c == 'n') n++;
         }
-        if(mp.size() != 5) return 0;
-        int minCount = Integer.MAX_VALUE;
-        for(char c: mp.keySet()){
-            if(c == 'l' || c =='o'){
-                mp.put(c,mp.get(c)/2);
-            }
-            minCount = Math.min(minCount,mp.get(c));
-        }
-        
-        return minCount ;
+
+        l /= 2;
+        o /= 2;
+
+        return Math.min(
+                Math.min(Math.min(b, a), l),
+                Math.min(o, n)
+        );
     }
 }
